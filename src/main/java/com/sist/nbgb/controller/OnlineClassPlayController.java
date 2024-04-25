57a1cb39ebec5c25f56176058c0c0acfb2783978
yeooon030 <tndus1132@naver.com>
2024-04-25 22:37:35 +0900

    2024/04/25 수연(로그인/재생CSS)

---
+++ src/main/java/com/sist/nbgb/controller/OnlineClassPlayController.java
@@ -0,0 +1,17 @@
+package com.sist.nbgb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Controller
+public class OnlineClassPlayController {
+	
+	//온라인 강의 재생 페이지
+	@GetMapping("/onlinePlay")
+	public String onlinePlay() {
+		return "onlineClass/onlineClassPlay";
+	}
+}
