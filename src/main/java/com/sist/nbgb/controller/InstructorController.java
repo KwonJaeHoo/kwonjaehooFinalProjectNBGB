857bec61441e3c00bc47e00dfc98357f4e938255
KwonJaeHoo <rainmiro@naver.com>
2024-04-26 09:22:59 +0900

    2024-04-26-재후

---
+++ src/main/java/com/sist/nbgb/controller/InstructorController.java
@@ -0,0 +1,21 @@
+package com.sist.nbgb.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.sist.nbgb.service.InstructorsService;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class InstructorController 
+{
+	private final InstructorsService instructorsService;
+	
+	@GetMapping("/instructor/intest")
+	public ResponseEntity<String> instTest()
+	{
+		return ResponseEntity.ok("asdsd");
+	}
+}
--- src/main/java/com/sist/nbgb/service/InstructorsService.java
+++ src/main/java/com/sist/nbgb/service/InstructorsService.java
@@ -1,26 +1,20 @@
 package com.sist.nbgb.service;
 
-import java.time.LocalDateTime;
-
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.sist.nbgb.dto.InstructorIdCheckDto;
-import com.sist.nbgb.dto.InstructorsDto;
-
-import com.sist.nbgb.entity.Instructors;
-import com.sist.nbgb.enums.Role;
-import com.sist.nbgb.enums.Status;
 import com.sist.nbgb.repository.InstructorsRepository;
-
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class InstructorsService 
 {
-
+	private final InstructorsRepository instructorsRepository;
 	
-	
+	public InstructorIdCheckDto findByInstructorId(InstructorIdCheckDto instructorIdCheckDto)
+	{
+		return instructorsRepository.findByInstructorId(instructorIdCheckDto.getInstructorId())
+				.map(InstructorIdCheckDto :: of)
+				.orElseThrow(() -> new RuntimeException("로그인 유저 정보가 없습니다."));
+	}
 }
