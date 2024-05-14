c52afa52db42dca9f1bc1ce107d169e8e1daf6c5
KwonJaeHoo <rainmiro@naver.com>
2024-05-14 14:07:16 +0900

    20240514 1407 재후 (수정)

--- src/main/java/com/sist/nbgb/repository/ReferenceAnswerRepository.java
+++ src/main/java/com/sist/nbgb/repository/ReferenceAnswerRepository.java
@@ -2,10 +2,9 @@ package com.sist.nbgb.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.sist.nbgb.entity.Reference;
 import com.sist.nbgb.entity.ReferenceAnswer;
 
-public interface ReferenceAnswerRepository extends JpaRepository<ReferenceAnswer, Reference>
+public interface ReferenceAnswerRepository extends JpaRepository<ReferenceAnswer, Long>
 {
 	//ReferenceAnswer findByReference_referenceId(Reference reference);
 }
